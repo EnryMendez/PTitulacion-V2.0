e8fda356beaf88e995d0f0af970dbc953503f749
Enrique Mendez <henry_191220@hotmail.com>
2020-05-09 21:57:20 -0500

    Cambio

--- Proyecto_Sebet/Proyecto_Sebet/Controllers/HomeController.cs
+++ Proyecto_Sebet/Proyecto_Sebet/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace Proyecto_Sebet.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
